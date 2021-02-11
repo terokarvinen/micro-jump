a23dcaf41f93ba75b6f13f632e05f5b662fe9a20
Tero Karvinen <teroweb@gmx.com>
2021-02-11 17:51:46 +0200

    Add 0.0.2 version of plugin

---
+++ jump.lua
@@ -0,0 +1,19 @@
+-- jumptag - Jump to any function, class or heading with F4. Go, Python, C...
+-- Copyright 2020-2021 Tero Karvinen http://TeroKarvinen.com
+-- https://github.com/terokarvinen/micro-jump
+
+local config = import("micro/config")
+local shell = import("micro/shell")
+
+function init()
+	config.MakeCommand("jumptag", jumptagCommand, config.NoComplete)
+	config.TryBindKey("F4", "command:jumptag", true)
+end
+
+function jumptagCommand(bp) -- bp BufPane
+		local filename = bp.Buf.GetName(bp.Buf)
+		local cmd = string.format("bash -c \"ctags -f - --fields=n '%s'|fzf|tr ':' '\n'|tail -1\"", filename)
+		local out = shell.RunInteractiveShell(cmd, false, true)
+		local linenum = tonumber(out)-1
+		bp.Cursor.Y = linenum
+end
