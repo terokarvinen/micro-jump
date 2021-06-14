ce78c6d1e684eba1a665a69898171b49c48bf235
Tero Karvinen <teropkg@gmx.com>
2021-06-14 07:32:41 +0300

    Correct order for Markdown headings; Handle cancelled jump cleanly

--- jump.lua
+++ jump.lua
@@ -1,9 +1,11 @@
 -- jumptag - Jump to any function, class or heading with F4. Go, Python, C...
 -- Copyright 2020-2021 Tero Karvinen http://TeroKarvinen.com
 -- https://github.com/terokarvinen/micro-jump
+-- MIT license
 
 local config = import("micro/config")
 local shell = import("micro/shell")
+local micro = import("micro")
 
 function init()
 	config.MakeCommand("jumptag", jumptagCommand, config.NoComplete)
@@ -12,8 +14,13 @@ end
 
 function jumptagCommand(bp) -- bp BufPane
 		local filename = bp.Buf.Path
-		local cmd = string.format("bash -c \"ctags -f - --fields=n '%s'|fzf|tr ':' '\n'|tail -1\"", filename)
+		local cmd = string.format("bash -c \"ctags -f - --fields=n '%s'|fzf --layout=reverse|tr ':' '\n'|tail -1\"", filename)
 		local out = shell.RunInteractiveShell(cmd, false, true)
+		if tonumber(out) == nil then
+			micro.InfoBar():Message("Jump cancelled.")
+			return
+		end
 		local linenum = tonumber(out)-1
 		bp.Cursor.Y = linenum
+		micro.InfoBar():Message(string.format("Jumped to line ", linenum))
 end
