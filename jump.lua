344ca9daab30c2fa8849a0ca41966629147347b1
Tero Karvinen <teropkg@gmx.com>
2024-03-28 15:58:39 +0200

    Improve error messages, clarify that exuberant-ctags is required - not emacs ctags.

--- jump.lua
+++ jump.lua
@@ -1,7 +1,11 @@
 -- jumptag - Jump to any function, class or heading with F4. Go, Python, C...
--- Copyright 2020-2022 Tero Karvinen http://TeroKarvinen.com
+-- Copyright 2020-2023 Tero Karvinen http://TeroKarvinen.com
 -- https://github.com/terokarvinen/micro-jump
 -- MIT license
+
+-- Requires exuberant-ctags:	sudo apt-get -y install exuberant-ctags
+-- Does not work with emacs ctags, which is in default 
+-- install of Debian 12-bookworm: "ctags: unrecognized option '--sort=no'"
 
 local config = import("micro/config")
 local shell = import("micro/shell")
@@ -17,12 +21,14 @@ function jumptagCommand(bp) -- bp BufPane
 		local filename = bp.Buf.Path
 		local cmd = string.format("bash -c \"ctags -f - --sort=no --fields=n '%s'|fzf --layout=reverse|tr ':' '\n'|tail -1\"", filename)
 		-- --sort=no shows symbols in file order, creating a TOC for Markdown files
-		local out = shell.RunInteractiveShell(cmd, false, true)
+		local out, err = shell.RunInteractiveShell(cmd, false, true)
 		if tonumber(out) == nil then
-			micro.InfoBar():Message("Jump cancelled.")
+			local msg = "Jump cancelled."
+			-- msg = string.format("%s Command '%s', out: '%s', err: '%s' ", msg, cmd, out, err) -- stderr shown on terminal after closing micro
+			micro.InfoBar():Message(msg)
 			return
 		end
 		local linenum = tonumber(out)-1
 		bp.Cursor.Y = linenum
-		micro.InfoBar():Message(string.format("Jumped to line ", linenum))
+		micro.InfoBar():Message(string.format("Jumped to line %i", linenum))
 end
